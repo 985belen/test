8ee0f2b69efc703983a343c744e8af470592fcab
zcy <18339694416@163.com>
2018-08-17 15:53:13 +0800

    的v

---
+++ src/view/iframe/ProductSeries.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="target" style="width:100%;height:100%;overflow:hidden;">
+    <iframe src='http://localhost:4848/single?appid=D:\Qlik\Pricing Profit Margin 4.qvf&sheet=50f1b6e8-652c-4173-9f5c-90b5f113afb9&opt=currsel&select=clearall' style='border:none;width:1500px;height:800px'>
+      <p>你的浏览器不支持iframe标签</p>
+    </iframe>
+  </div>
+</template>
+<script>
+export default {
+}
+</script>
+<style>
+</style>
---
+++ src/view/iframe/ProductSummary.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="target" style="width:100%;height:100%;overflow:hidden;">
+    <iframe src='http://localhost:4848/single?appid=D:\Qlik\Pricing Profit Margin 4.qvf&sheet=4a3e5efa-8d62-47f2-ae6f-3be33279bae7&opt=currsel&select=clearall' style='border:none;width:1500px;height:800px'>
+      <p>你的浏览器不支持iframe标签</p>
+    </iframe>
+  </div>
+</template>
+<script>
+export default {
+}
+</script>
+<style>
+ 
+</style>
